6abd1d669539a23b343fb6b63ef6a704f4bcf0a8
Michael Nutt <michael@nuttnet.net>
2012-02-16 20:26:26 -0500

    upgrade redis client and socket.io

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-require.paths.unshift(__dirname + "/node_modules");
+var redis = require("redis"),
+  http = require("http"),
+  socketio = require("socket.io"),
+  client = redis.createClient();
 
+var io = socketio.listen(8080, "0.0.0.0")
+
+// Redis channel to listen on
 var channel = process.argv[2];
 if(!channel) {
   console.log("Usage: node app.js [redis channel]");
   process.exit(1);
 }
 
-var redis = require("redis"),
-  http = require("http"),
-  io = require("socket.io"),
-  client = redis.createClient();
-
-// Set up websockets
-var server = http.createServer(function(req, res) {
-  res.writeHeader(200, {'Content-Type': 'text/html'});
-  res.writeBody('<h1>Hello world</h1>');
-  res.finish();
-});
-server.listen(8080, "0.0.0.0")
-var socket = io.listen(server);
-socket.on("connection", function(client) {
+// For debugging only
+io.sockets.on("connection", function(socket) {
   console.log("New client!");
 });
 
-// Watch redis
-console.log("Watching channel: " + channel);
-
 client.on("message", function(channel, message) {
-  console.log(message.toString());
-  socket.broadcast(message.toString());
+  // You could emit this on a uid rather than 'message' to send
+  // different messages to different users
+  io.sockets.emit('message', message.toString());
 });
 
+// Watch redis
 client.subscribe(channel);
+console.log("Watching channel: " + channel);
